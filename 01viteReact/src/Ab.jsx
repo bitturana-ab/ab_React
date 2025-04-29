2c9c9120ca98c3943575c3c73e3632b8129ba990
AB <AB@AB.com>
2025-04-29 15:16:04 +0530

    Initialize Vite React project with essential files and configurations
    - Created package.json with dependencies and scripts for development, build, and linting.
    
    - Implemented Ab component that displays a waiting message.
    - Developed App component to render Ab and a project title.
    - Set up main entry point for React application with StrictMode.
    - Configured Vite with React plugin for optimal performance.

---
+++ 01viteReact/src/Ab.jsx
@@ -0,0 +1,5 @@
+function Ab() {
+  return <h3>I'm waiting.</h3>;
+}
+
+export default Ab;
---
+++ 01viteReact/src/App.jsx
@@ -0,0 +1,12 @@
+import Ab from "./Ab";
+function App() {
+  return (
+    <>
+      <Ab/>
+      <h1>A New Project</h1>
+    </>
+    
+  )
+}
+
+export default App
