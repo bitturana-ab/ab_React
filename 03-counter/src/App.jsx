e023ae58a95d4425a508979331f40962e6e8ae81
AB <AB@AB.com>
2025-05-25 16:33:55 +0530

    hooks for counter

---
+++ 03-counter/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let counter = 0
+  const addValue =()=>{
+    console.log("value added",Math.random());
+    counter++;
+  }
+  return (
+    <>
+    <h1>A new vite project</h1>
+    <h2>Counter value: {counter}</h2>
+    <button onClick={addValue}>Increase value</button>
+    <button>Decrease value</button>
+    </>
+  )
+}
+
+export default App
