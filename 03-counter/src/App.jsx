40b9324ab22a020df21b2f8be52407f8e3bd2651
AB <AB@AB.com>
2025-05-26 14:16:27 +0530

    useState for counter update in reactJs

--- 03-counter/src/App.jsx
+++ 03-counter/src/App.jsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
 
-  let counter = 0
+  let [counter,updateCounter] = useState(0);
+
+  // let counter = 0
   const addValue =()=>{
-    console.log("value added",Math.random());
-    counter++;
+    if(counter == 28) updateCounter(28);
+    else{
+      counter++;
+      console.log(counter)
+      updateCounter(counter);
+    }
+  }
+  const lessValue=()=>{
+    if(counter == 0) updateCounter(0);
+    else updateCounter(counter--);
   }
   
   return (
@@ -14,7 +24,7 @@ function App() {
     <h1>A new vite project</h1>
     <h2>Counter value: {counter}</h2>
     <button onClick={addValue}>Increase value</button>
-    <button>Decrease value</button>
+    <button onClick={lessValue}>Decrease value</button>
     </>
   )
 }
