07a201ddb224d81801c83a7614c91e96a79aa8dc
bitturana-ab <b2ra830@gmail.com>
2025-06-06 11:41:04 +0530

    darkMode by class||media all sets

--- 09-themeSwitcher/src/App.jsx
+++ 09-themeSwitcher/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [themeMode,setTheme] = useState("light");
+  const lightTheme=()=>{
+    setTheme("light");
+  }
+  const darkTheme=()=>{
+    setTheme("dark");
+  }
+  // actual change in theme 
+  useEffect(()=>{
+    const htmlPage = document.querySelector('html');
+    // first remove all mode we don't know what present there
+    htmlPage.classList.remove("light","dark");
+    htmlPage.classList.add(themeMode);
+    
+  },[themeMode])
   return (
-    <>
-      <h1 className='p-3 bg-pink-400 text-black font-sans'>Theme switcher project</h1>
-    </>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+            <div className="flex flex-wrap min-h-screen dark:bg-gray-800 dark:border-gray-700 items-center ">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* theme buttom */}
+                        <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* card */}
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+    </ThemeProvider>
   )
 }
 
