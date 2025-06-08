e48e13fc3eabdb6ffd0fe2f9f3bb12ae6e4edf73
bitturana-ab <b2ra830@gmail.com>
2025-06-08 15:07:22 +0530

    github page optimized

--- 07-reactRouter/src/components/Github/Github.jsx
+++ 07-reactRouter/src/components/Github/Github.jsx
@@ -13,7 +13,9 @@ function Github() {
   return (
     <>
     <div className='bg-gray-500 text-white p-4 text-3xl justify-center'>Github followers: {data.name}
-    <img src={data.avatar_url} width={300} alt="Git picture" className='p-5 justify-center'/></div>
+    <img src={data.avatar_url} width={300} alt="Git picture" className='p-5 justify-center'/>
+    <p className="p-3 bg-red-200 text-black mt-3 mb-4">Github data : {data.login}</p>
+        < p className="p-3 bg-blue-300 text-black">Github Bio : {data.bio}</p></div>
     </>
   )
 }
